0fc693bec22d5320563a2f85c92d08e912cc93e2
Renato Carvalho Silva <rsilv1822@gmail.com>
2024-07-30 23:06:15 -0300

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import Navbar from "./components/Navbar/index.tsx";
+import Listing from "./pages/Listing/index.tsx";
+import Form from "./pages/Form/index.tsx";
 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Listing() {
+
+    return(
+        <h1>Página de listagem</h1>
+    );
+}
+
+export default Listing;
