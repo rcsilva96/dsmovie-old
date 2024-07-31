9644bbb60de2b7036362669650caf23d9fc32cf9
Renato Carvalho Silva <rsilv1822@gmail.com>
2024-07-31 00:05:02 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './styles.css'
+import { Link } from "react-router-dom";
 
 function Form() {
   const movie = {
@@ -37,7 +38,12 @@ function Form() {
             </button>
           </div>
         </form>
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+
+        <Link to="/">
+
+          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        
+        </Link>
       </div>
     </div>
   );
