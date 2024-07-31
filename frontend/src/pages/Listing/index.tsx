617901a8c07ab8cc8a8fb751d2f1193a627cc930
Renato Carvalho Silva <rsilv1822@gmail.com>
2024-07-30 23:56:29 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,12 +1,39 @@
 import React from "react";
 import Pagination from "../../components/Pagination/index.tsx";
-import './styles.css';
+import "./styles.css";
+import MovieCard from "../../components/MovieCard/index.tsx";
 
 function Listing() {
+  return (
+    <>
+      <Pagination />
 
-    return(
-        <Pagination />
-    );
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
